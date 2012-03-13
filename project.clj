067b57fa1465b023fc1849dc7e316a44c0cca132
chapmanb <chapmanb@50mail.com>
2012-03-13 07:33:56 -0400

    Add lein midje dev dependency for testing

--- project.clj
+++ project.clj
@@ -14,7 +14,8 @@
                  [compojure "1.0.1"]
                  [ring "1.0.2"]
                  [enlive "1.0.0"]]
-  :dev-dependencies [[midje "1.3.0" :exclusions [org.clojure/clojure]]]
+  :dev-dependencies [[midje "1.3.0" :exclusions [org.clojure/clojure]]
+                     [lein-midje "1.0.8"]]
   :java-source-path "src/java"
   :omit-source true
   :aot [bcbio.variation.vcfwalker bcbio.variation.core bcbio.variation.annotate.nbq]
