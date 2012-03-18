cd2e8b7a7004ac035c6705a710eeb8b31b836c02
chapmanb <chapmanb@50mail.com>
2012-03-18 17:28:32 -0400

    Provide lenient stringency for Picard when indexing BAM file

--- src/bcbio/run/broad.clj
+++ src/bcbio/run/broad.clj
@@ -1,7 +1,7 @@
 (ns bcbio.run.broad
   "High level functions to run software from Broad: GATK, Picard"
   (:import [org.broadinstitute.sting.gatk CommandLineGATK]
-           [net.sf.samtools SAMFileReader]
+           [net.sf.samtools SAMFileReader SAMFileReader$ValidationStringency]
            [net.sf.picard.sam BuildBamIndex])
   (:use [clojure.java.io])
   (:require [fs.core :as fs]
@@ -23,5 +23,7 @@
   [in-bam]
   (let [index-file (str in-bam ".bai")]
     (if (itx/needs-run? index-file)
-      (BuildBamIndex/createIndex (SAMFileReader. (file in-bam)) (file index-file)))
+      (do
+        (SAMFileReader/setDefaultValidationStringency SAMFileReader$ValidationStringency/LENIENT)
+        (BuildBamIndex/createIndex (SAMFileReader. (file in-bam)) (file index-file))))
     index-file))
