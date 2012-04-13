2900a9cd4dd1e9ae9ecbe1d49d039615a4e8ed68
chapmanb <chapmanb@50mail.com>
2012-04-13 09:43:34 -0400

    Annotate VCF files with Complete Genomics metrics from masterVar files

---
+++ src/bcbio/variation/utils/cgmetrics.clj
@@ -0,0 +1,81 @@
+(ns bcbio.variation.utils.cgmetrics
+  "Add metrics from Complete Genomics masterVar file to a VCF.
+  This updates a converted VCF from Complete Genomics with metrics information
+  allowing assessment and filtering."
+  (:import [org.broadinstitute.sting.utils.variantcontext VariantContextBuilder]
+           [org.broadinstitute.sting.utils.codecs.vcf VCFHeader VCFInfoHeaderLine
+            VCFHeaderLineCount VCFHeaderLineType])
+  (:use [clojure.java.io]
+        [ordered.set :only (ordered-set)]
+        [bcbio.variation.normalize :only [hg19-map]]
+        [bcbio.variation.variantcontext :only [parse-vcf write-vcf-w-template
+                                               get-vcf-source]])
+  (:require [clojure.data.csv :as csv]
+            [bcbio.run.itx :as itx]))
+
+(defn- get-masterVar-metrics
+  "Get lookup dictionary of CG variant metrics by position."
+  [in-file]
+  (letfn [(variant-score [line name]
+            (let [alleles ["1" "2"]]
+              (/ (apply + (map #(Float/parseFloat (get line (format "allele%sVarScore%s" % name)))
+                               alleles))
+                 (count alleles))))
+          (allele-balance [line]
+            (/ (Float/parseFloat (get line "referenceAlleleReadCount"))
+               (Float/parseFloat (get line "totalReadCount"))))]
+    (with-open [rdr (reader in-file)]
+      (let [csv-iter (drop-while #(< (count %) 3)
+                                 (csv/read-csv rdr :separator \tab))
+            header (first csv-iter)]
+        (reduce (fn [coll xs]
+                  (let [line (zipmap header xs)]
+                    (assoc coll [(get hg19-map (get line "chromosome"))
+                                 (dec (Integer/parseInt (get line "begin")))]
+                           {:depth (get line "totalReadCount")
+                            :qual-eaf (variant-score line "EAF")
+                            :qual-vaf (variant-score line "VAF")
+                            :ab (allele-balance line)})))
+                {} (rest csv-iter))))))
+
+(defn- add-cgmetrics-iter
+  "Provide iterator of variants with CG metrics added"
+  [vcf-source metrics]
+  (letfn [(update-cgmetrics [vc x]
+            (-> (VariantContextBuilder. (:vc vc))
+                (.attributes (assoc (:attributes vc)
+                               "DP" (:depth x)
+                               "AB" (:ab x)
+                               "QUALEAF" (:qual-eaf x)
+                               "QUALVAF" (:qual-vaf x)))
+                .make))]
+    (map (fn [vc]
+           (if-let [cur-metrics (get metrics [(:chr vc) (:start vc)])]
+             (update-cgmetrics vc cur-metrics)
+             (:vc vc)))
+         (parse-vcf vcf-source))))
+
+(defn- add-cgmetrics-header
+  "Add CG metrics definitions to the VCF input header."
+  [header]
+  (let [new #{(VCFInfoHeaderLine. "DP" 1
+                                  VCFHeaderLineType/Integer "Total Depth")
+              (VCFInfoHeaderLine. "QUALEAF" 1
+                                  VCFHeaderLineType/Float "Variant Quality EAF")
+              (VCFInfoHeaderLine. "QUALVAF" 1
+                                  VCFHeaderLineType/Float "Variant Quality VAF")
+              (VCFInfoHeaderLine. "AB" 1
+                                  VCFHeaderLineType/Float "Allele Balance")}]
+    (VCFHeader. (apply ordered-set (concat (.getMetaData header) new))
+                (.getGenotypeSamples header))))
+
+(defn add-cgmetrics
+  "Add metrics from Complete Genomics masterVar file to VCF."
+  [vcf-file mastervar-file ref-file & {:keys [out-dir]}]
+  (let [out-file (itx/add-file-part vcf-file "cgmetrics" out-dir)
+        metrics (get-masterVar-metrics mastervar-file)]
+    (with-open [vcf-source (get-vcf-source vcf-file ref-file)]
+      (write-vcf-w-template vcf-file {:out out-file}
+                            (add-cgmetrics-iter vcf-source metrics)
+                            ref-file :header-update-fn add-cgmetrics-header))
+    out-file))
---
+++ test/bcbio/variation/test/utils.clj
@@ -0,0 +1,21 @@
+(ns bcbio.variation.test.utils
+  (:use [midje.sweet]
+        [bcbio.variation.normalize :only [prep-vcf]]
+        [bcbio.variation.utils.cgmetrics])
+  (:require [fs.core :as fs]
+            [bcbio.run.itx :as itx]))
+
+(background
+ (around :facts
+         (let [data-dir (str (fs/file "." "test" "data"))
+               ref (str (fs/file data-dir "GRCh37.fa"))
+               cg-vcf (str (fs/file data-dir "cg-normalize.vcf"))
+               cg-var (str (fs/file data-dir "cg-masterVar-NA19239.tsv"))
+               out-cg-var (itx/add-file-part cg-vcf "prep-cgmetrics")]
+           (doseq [x [out-cg-var]]
+             (itx/remove-path x))
+           ?form)))
+
+(facts "Add Complete Genomics metrics to VCF file."
+  (let [ready-vcf (prep-vcf cg-vcf ref "NA12939" :sort-pos true)]
+    (add-cgmetrics ready-vcf cg-var ref) => out-cg-var))
