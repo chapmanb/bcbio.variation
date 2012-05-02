93b196cbdd0ad6ec7e5009111a0afdfd4c0e2516
chapmanb <chapmanb@50mail.com>
2012-05-02 15:59:40 -0400

    Convert diploid calls on fosmid regions into haploid variants, keeping heterozygotes based on alternative homozygous variant likelihood

---
+++ src/bcbio/variation/haploid.clj
@@ -0,0 +1,56 @@
+(ns bcbio.variation.haploid
+  "Convert diploid variants from GATK into haploid calls based on genotype likelihoods.
+  We assess diploid GATK calls based on the phred-normalized likelihood (PL). Lower variant
+  PLs are likely to be true and included. The GATK documentation contains a detailed example
+  of the format and interpretation:
+  http://www.broadinstitute.org/gsa/wiki/index.php/
+  Understanding_the_Unified_Genotyper%27s_VCF_files#How_genotypes_are_represented_in_a_VCF"
+  (:import [org.broadinstitute.sting.utils.variantcontext 
+            VariantContextBuilder GenotypesContext Genotype])
+  (:use [bcbio.variation.variantcontext :only [parse-vcf get-vcf-source write-vcf-w-template]])
+  (:require [bcbio.run.itx :as itx]))
+
+(def ^{:doc "Threshold to include a heterozygous allele as a haploid homozygote variant."}
+  haploid-thresh 1e-5)
+
+(defn- get-haploid-genotype
+  "Retrieve updated genotype with haploid allele."
+  [vc]
+  (let [g (-> vc :genotypes first)]
+    (letfn [(maybe-variant-haploid [g]
+              (when (.hasLikelihoods g)
+                (let [in-map (-> (.getLikelihoods g) (.getAsMap true))
+                      variant-prob (get (zipmap (map #(.name %) (keys in-map)) (vals in-map))
+                                        "HOM_VAR")]
+                  (when (> variant-prob haploid-thresh)
+                    (first (filter #(and (.isNonReference %) (.isCalled %))
+                                   (.getAlleles g)))))))
+            (get-haploid-allele [g]
+              (case (:type g)
+                "HOM_VAR" (first (:alleles g))
+                "HET" (maybe-variant-haploid (:genotype g))
+                nil))]
+      (when-let [allele (get-haploid-allele g)]
+        (doto (-> vc :vc .getGenotypes GenotypesContext/copy)
+          (.replace (Genotype/modifyAlleles (:genotype g) [allele])))))))
+
+(defn- convert-to-haploid
+  "Convert diploid allele to haploid variant."
+  [vc]
+  {:pre (= 1 (count (:genotypes vc)))}
+  (when-let [genotype (get-haploid-genotype vc)]
+    (-> (VariantContextBuilder. (:vc vc))
+        (.genotypes genotype)
+        (.make))))
+
+(defn diploid-calls-to-haploid
+  "Convert set of diploid GATK calls on a haploid genome based on likelihoods."
+  [vcf ref & {:keys [out-dir]}]
+  (let [out-files {:out (itx/add-file-part vcf "haploid" out-dir)}]
+    (when (itx/needs-run? (vals out-files))
+      (with-open [vcf-source (get-vcf-source vcf ref)]
+        (write-vcf-w-template vcf out-files
+                              (remove nil?
+                                      (map convert-to-haploid (parse-vcf vcf-source)))
+                              ref)))
+    (:out out-files)))
--- test/bcbio/variation/test/validate.clj
+++ test/bcbio/variation/test/validate.clj
@@ -1,6 +1,7 @@
 (ns bcbio.variation.test.validate
   "Final variant filtration and preparation for validation."
   (:use [midje.sweet]
+        [bcbio.variation.haploid]
         [bcbio.variation.validate])
   (:require [fs.core :as fs]
             [bcbio.run.itx :as itx]))
@@ -10,8 +11,10 @@
          (let [data-dir (str (fs/file "." "test" "data"))
                ref (str (fs/file data-dir "GRCh37.fa"))
                top-vcf (str (fs/file data-dir "gatk-calls.vcf"))
-               top-out (itx/add-file-part top-vcf "topsubset")]
-           (doseq [x (concat [top-out])]
+               top-out (itx/add-file-part top-vcf "topsubset")
+               dip-vcf (str (fs/file data-dir "phasing-input-diploid.vcf"))
+               dip-out (itx/add-file-part dip-vcf "haploid")]
+           (doseq [x (concat [top-out dip-out])]
              (itx/remove-path x))
            ?form)))
 
@@ -20,3 +23,6 @@
                                      :top-metric [{:name "QD" "mod" 1}]}}}]
   (facts "Prepare top variants sorted by metrics."
     (get-to-validate top-vcf finalizer ref) => top-out))
+
+(facts "Convert diploid calls into haploid reference variants."
+  (diploid-calls-to-haploid dip-vcf ref) => dip-out)
