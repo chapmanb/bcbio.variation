a5891c3a2e34e3c6b59ef9d4405c16de35e0ffae
chapmanb <chapmanb@50mail.com>
2012-06-08 11:55:10 -0400

    Allow checks for variants in BED interval files when summarizing multi-samples VCFs

--- src/bcbio/variation/utils/summarize.clj
+++ src/bcbio/variation/utils/summarize.clj
@@ -2,6 +2,7 @@
   "Collapse a multi-sample VCF file into a CSV, R data.frame ready, parameter summary."
   (:use [clojure.java.io]
         [ordered.map :only [ordered-map]]
+        [bcbio.variation.callable :only [get-bed-source features-in-region]]
         [bcbio.variation.config :only [load-config]]
         [bcbio.variation.variantcontext :only [parse-vcf get-vcf-source]])
   (:require [clojure.data.csv :as csv]
@@ -37,27 +38,44 @@
   (reduce (fn [coll k] (assoc coll k (get-in vc [:attributes k])))
           out attrs))
 
+(defn- flatten-vc-intervals
+  "Check for presence of the variant in predefined intervals."
+  [out vc intervals]
+  (letfn [(check-intervals [vc bed-s]
+            (if (empty? (features-in-region bed-s (:chr vc) (:start vc) (:end vc))) 0 1))]
+    (reduce (fn [coll interval]
+              (assoc coll (:name interval) (check-intervals vc (:source interval))))
+            out intervals)))
+
 (defn- flatten-vc
   "Provide tabular variant representation with provided attributes and sample information."
-  [attrs sample-attrs vc]
+  [config vc]
   (-> (reduce (fn [coll k] (assoc coll k (get vc k)))
               (ordered-map) [:chr :start :id :type :qual])
-      (flatten-vc-attrs vc attrs)
-      (flatten-vc-samples vc sample-attrs)))
+      (flatten-vc-intervals vc (get config :intervals []))
+      (flatten-vc-attrs vc (:attrs config))
+      (flatten-vc-samples vc (:sample-attrs config))))
+
+(defn- add-interval-retrievers
+  [config]
+  (letfn [(add-int-retriever [coll]
+            (assoc coll :source (get-bed-source (:file coll))))]
+    (assoc config :intervals (map add-int-retriever (:intervals config)))))
 
 (defn vcf-to-table
   "Convert a VCF input to flattened CSV table with provided attributes."
-  [vcf ref attrs sample-attrs]
+  [vcf ref config]
   (let [out-file (str (itx/file-root vcf) "-variantsum.csv")]
     (when (itx/needs-run? out-file)
       (with-open [vcf-source (get-vcf-source vcf ref)
                   wtr (writer out-file)]
         (doseq [[i out] (map-indexed vector
-                                     (map (partial flatten-vc attrs sample-attrs)
+                                     (map (partial flatten-vc (add-interval-retrievers config))
                                           (parse-vcf vcf-source)))]
           (when (= i 0)
             (csv/write-csv wtr [(map name (keys out))]))
-          (csv/write-csv wtr [(vals out)]))))
+          (csv/write-csv wtr [(vals out)])
+          (.flush wtr))))
     out-file))
 
 (defn vcf-to-table-config
@@ -68,8 +86,7 @@
      (flatten
       (for [exp (:experiments config)]
         (for [call (:calls exp)]
-          (vcf-to-table (:file call) (:ref exp) (get-in call [:summary :attrs])
-                        (get-in call [:summary :sample-attrs]))))))))
+          (vcf-to-table (:file call) (:ref exp) (:summary call))))))))
 
 (defn -main [config-file]
-  (vcf-to-table config-file))
+  (vcf-to-table-config config-file))
