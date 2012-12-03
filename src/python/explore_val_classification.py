4c273ec303a60177ab0f10d3f7ee6cd817e0e935
chapmanb <chapmanb@50mail.com>
2012-12-03 13:44:39 -0500

    Explore machine learning classification approaches for separating possible true/false positives: evaluation logistic regression, SVM linear/rbf with parameter search and random forest

--- src/python/explore_val_classification.py
+++ src/python/explore_val_classification.py
@@ -5,19 +5,122 @@ This identifies useful classification metrics to distinguish
 true and false variants.
 """
 import os
+import math
 import shutil
+import itertools
 
 import vcf
+import numpy as np
 import pandas
 import ramp
+from ramp.estimators.sk import BinaryProbabilities
 import sklearn
+
+base_dir = "/usr/local/projects/xprize/quaternary"
+prep_dir = os.path.join(base_dir, "NA12878_fosmid/work/prep")
+mp_tps_vcf = os.path.join(prep_dir, "NA12878-allfos-nomnp-fullcombine-wrefs-cleaned-annotated-ffilter-nosv-nofilter-tps.vcf")
+mp_fps_vcf = os.path.join(prep_dir, "NA12878-allfos-nomnp-fullcombine-wrefs-cleaned-annotated-ffilter-nosv-nofilter-fps.vcf")
 
 def main():
     tp_vcf = "NA12878-validate-tps.vcf"
     fp_vcf = "NA12878-validate-fps.vcf"
+    metrics = ['DP', 'Entropy', 'FS', 'GC', 'HRun', 'HaplotypeScore', 'MFE',
+               'MQ', 'NBQ', 'ReadPosEndDist']
+    format_metrics = ["AD", "PL", "QUAL"]
+    sanger_decisiontree(tp_vcf, fp_vcf, metrics, format_metrics)
+    ml_params(mp_tps_vcf, mp_fps_vcf, metrics, format_metrics)
+
+# ## Machine learning parameters
+
+def ml_params(tp_vcf, fp_vcf, metrics, format_metrics):
+    """Explore machine learning parameters to identify approaches to help separate data.
+    """
+    metrics = ['Entropy', 'FS', 'HRun', 'MFE',
+               'MQ', 'NBQ', 'ReadPosEndDist']
+    exploring = True
+    with open(tp_vcf) as in_handle:
+        df_tp = read_vcf_metrics(in_handle, metrics, format_metrics, 1,
+                                 exploring)
+    with open(fp_vcf) as in_handle:
+        df_fp = read_vcf_metrics(in_handle, metrics, format_metrics, -1,
+                                 exploring)
+    df = pandas.concat([df_tp, df_fp], keys=["tp", "fp"])
+    if exploring:
+        df = df.fillna({"NBQ": df["NBQ"].mean(), "PL" : df["PL"].mean(),
+                        "AD" : df["AD"].mean(), "FS": 0.0})
+    for val, name in [(0, "snp"), (1, "indel")]:
+        print "--->", name
+        ml_param_explore(df[df["indel"] == val], metrics + format_metrics,
+                         exploring)
+
+def ml_param_explore(df, metrics, test_all=False):
+    """Explore classification approaches and parameters, leveraging ramp to compare multiple models.
+    """
+    print df.describe()
+    context = ramp.DataContext(data=df)
+    config = ramp.Configuration(target="target", metrics=[ramp.metrics.AUC(), ramp.metrics.F1(),
+                                                          ramp.metrics.HingeLoss()])
+    #rf_params = _find_rf_params(df, metrics)
+    models = [sklearn.ensemble.RandomForestClassifier(n_estimators=50,
+                                                      max_features=int(math.ceil(math.sqrt(len(metrics)))))]
+    if test_all:
+        rbf_params = _find_svm_rbf_params(df, metrics)
+        models += [sklearn.svm.SVC(kernel="linear"),
+                   sklearn.linear_model.LogisticRegression(),
+                   sklearn.svm.SVC(kernel="rbf", C=rbf_params["C"], gamma=rbf_params["gamma"])]
+    factory = ramp.ConfigFactory(config,
+                                 features=[[ramp.BaseFeature(x) for x in metrics]],
+                                 model = models)
+    for x in factory:
+        ramp.models.cv(x, context, folds=5, repeat=2,
+                       print_results=True)
+
+def _find_rf_params(df, metrics):
+    """Perform a grid search to find best parameters for random forest.
+    """
+    context = ramp.DataContext(data=df)
+    config = ramp.Configuration(target="target",
+                                features=[ramp.BaseFeature(x) for x in metrics])
+    x, y = ramp.models.get_xy(config, context)
+
+    n = len(metrics)
+    param_grid = dict(max_features=range(int(math.ceil(math.sqrt(n))), n+1, 3),
+                      n_estimators=range(20, 101, 20))
+    grid = sklearn.grid_search.GridSearchCV(sklearn.ensemble.RandomForestClassifier(),
+                                            param_grid=param_grid,
+                                            cv=sklearn.cross_validation.StratifiedKFold(y=y, k=3))
+    grid.fit(x, y)
+    print grid.best_estimator_
+    out = {}
+    for attr in param_grid.keys():
+        out[attr] = getattr(grid.best_estimator_, attr)
+    return out
+
+def _find_svm_rbf_params(df, metrics):
+    """Perform a grid search to find best parameters for a SVM RBF kernel.
+    """
+    context = ramp.DataContext(data=df)
+    config = ramp.Configuration(target="target",
+                                features=[ramp.BaseFeature(x) for x in metrics])
+    x, y = ramp.models.get_xy(config, context)
+
+    param_grid = dict(gamma=10.0 ** np.arange(-5, 4),
+                      C=10.0 ** np.arange(-2, 9))
+    grid = sklearn.grid_search.GridSearchCV(sklearn.svm.SVC(kernel="rbf"),
+                                            param_grid=param_grid,
+                                            cv=sklearn.cross_validation.StratifiedKFold(y=y, k=3))
+    grid.fit(x, y)
+    print grid.best_estimator_
+    return {"C": grid.best_estimator_.C, "gamma": grid.best_estimator_.gamma}
+
+# ## Decision tree visualization
+
+def sanger_decisiontree(tp_vcf, fp_vcf, metrics, format_metrics):
+    """Explore sanger true/false calls with a decision tree.
+
+    This helps identify primary metrics helping to discriminate the data.
+    """
     out_decision = "decision-tree-%s.graphviz"
-    #metrics = ['DP', 'Entropy', 'FS', 'GC', 'HRun', 'HaplotypeScore', 'MFE',
-    #           'MQ', 'NBQ', 'ReadPosEndDist']
     metrics = ['FS', 'MFE',
                'NBQ', 'ReadPosEndDist']
     format_metrics = ["AD", "PL", "QUAL"]
@@ -33,12 +136,7 @@ def main():
     print df_fp.describe()
 
 def explore_ml_decisiontree(df, metrics, out_decision):
-    store_dir = os.path.join(os.getcwd(), "ramp")
-    if os.path.exists(store_dir):
-        shutil.rmtree(store_dir)
-    os.makedirs(store_dir)
-    context = ramp.DataContext(store=os.path.join(os.getcwd(), "ramp"),
-                               data=df)
+    context = ramp.DataContext(data=df)
     config = ramp.Configuration(target="target", metrics=[ramp.metrics.AUC()])
     factory = ramp.ConfigFactory(config,
                                  features=[[ramp.BaseFeature(x) for x in metrics]],
@@ -48,12 +146,19 @@ def explore_ml_decisiontree(df, metrics, out_decision):
         sklearn.tree.export_graphviz(x.model, out_file=out_decision,
                                      feature_names=metrics)
 
-def read_vcf_metrics(in_handle, metrics, format_metrics, target):
+# ## Parse VCFs into pandas data frames
+
+def read_vcf_metrics(in_handle, metrics, format_metrics, target,
+                     use_subset = False):
     d = {"target": [],
          "indel": []}
     for x in metrics + format_metrics:
         d[x] = []
-    for rec in vcf.VCFReader(in_handle):
+    if use_subset:
+        recs = itertools.islice(vcf.VCFReader(in_handle), 5000)
+    else:
+        recs = vcf.VCFReader(in_handle)
+    for rec in recs:
         for x in metrics:
             d[x].append(rec.INFO.get(x, None))
         d["target"] = target
--- src/python/pull_tps_fps.py
+++ src/python/pull_tps_fps.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python
 """Extract true positives and negatives with metrics based on validation results.
+
+Also identifies incorrect true and false positives in training data sets to pull
+out areas where the approach to creating training sets could improve.
 """
 import os
-
-import vcf
 
 base_dir = "/usr/local/projects/xprize/quaternary"
 val_vcf = os.path.join(base_dir, "NA12878_validate/inputs/NA12878-sanger-validate-allowone.vcf")
 prep_dir = os.path.join(base_dir, "NA12878_fosmid/work/prep")
 full_vcf = os.path.join(prep_dir, "NA12878-allfos-nomnp-fullcombine-wrefs-cleaned-annotated.vcf")
+mp_tps_vcf = os.path.join(prep_dir, "NA12878-allfos-nomnp-fullcombine-wrefs-cleaned-annotated-ffilter-nosv-nofilter-tps.vcf")
+mp_fps_vcf = os.path.join(prep_dir, "NA12878-allfos-nomnp-fullcombine-wrefs-cleaned-annotated-ffilter-nosv-nofilter-fps.vcf")
 
 def main():
-    tp_out = "NA12878-validate-tps.vcf"
-    fp_out = "NA12878-validate-fps.vcf"
+    base = "NA12878-validate-%s.vcf"
+    tp_out = base % "tps"
+    fp_out = base % "fps"
     with open(val_vcf) as in_handle:
         tp_coords, fp_coords = read_validation(in_handle)
+        coords = {"fp": fp_coords, "tp": tp_coords}
     with open(tp_out, "w") as tp_handle:
         with open(fp_out, "w") as fp_handle:
             with open(full_vcf) as in_handle:
                 outs = {"fp": fp_handle, "tp": tp_handle}
-                coords = {"fp": fp_coords, "tp": tp_coords}
                 write_vcf_subsets(in_handle, outs, coords)
+    wrongtp_out = base % "train-wrongtps"
+    wrongfp_out = base % "train-wrongfps"
+    with open(wrongtp_out, "w") as out_handle:
+        with open(mp_tps_vcf) as in_handle:
+            write_incorrect_train(in_handle, out_handle, coords["fp"])
+    with open(wrongfp_out, "w") as out_handle:
+        with open(mp_fps_vcf) as in_handle:
+            write_incorrect_train(in_handle, out_handle, coords["tp"])
 
 def _get_coords(line):
     parts = line.split("\t")
     return (parts[0], parts[1])
+
+def write_incorrect_train(in_handle, out_handle, coords):
+    for line in in_handle:
+        if line.startswith("#"):
+            out_handle.write(line)
+        else:
+            if _get_coords(line) in coords:
+                out_handle.write(line)
 
 def write_vcf_subsets(in_handle, outs, coords):
     for line in in_handle:
