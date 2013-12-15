075357f41652f718e4a5d392e4e8e5d69957b237
chapmanb <chapmanb@50mail.com>
2013-12-15 14:35:49 -0500

    Allow specification of Illumina variant types to include when preparing from supplied VCFs

--- src/bcbio/variation/utils/illumina.clj
+++ src/bcbio/variation/utils/illumina.clj
@@ -5,7 +5,8 @@
    - Remove illegal gap characters from indel files.
    - Convert into GRCh37 sorted coordinates.
    - Merge SNP and Indels into single callset."
-  (:require [clojure.tools.cli :refer [cli]]
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [cli]]
             [me.raynes.fs :as fs]
             [bcbio.run.itx :as itx]
             [bcbio.variation.combine :refer [gatk-normalize]]
@@ -19,12 +20,11 @@
       first
       str))
 
-(defn get-vcf-files [path]
+(defn get-vcf-files [path vtypes]
   (if (.isDirectory (io/file path))
-      [(get-illumina-vcf path "SNPs")
-       (get-illumina-vcf path "Indels")
-       (get-illumina-vcf path "SVs")]
-      [path]))
+    (let [allowed-vtypes {:snp "SNPs" :indel "Indels" :sv "SVs"}]
+      (map #(get-illumina-vcf path (get allowed-vtypes %)) vtypes))
+    [path]))
 
 (defn prep-illumina-variants
   "Prepare Illumina variants from a standard directory structure.
@@ -35,8 +35,8 @@
     - orig-ref-file: Original reference file (hg19 for Illumina)
     - out-dir: Output directory to write to.
     - base-tmp-dir: Base temporary directory to work in."
-  [base-dir sample-name ref-file orig-ref-file out-dir base-tmp-dir]
-  (let [files (get-vcf-files base-dir)
+  [base-dir sample-name ref-file orig-ref-file out-dir base-tmp-dir vtypes]
+  (let [files (get-vcf-files base-dir vtypes)
         out-file (str (fs/file out-dir (str sample-name ".vcf")))]
     (when (itx/needs-run? out-file)
       (itx/with-temp-dir [tmp-dir base-tmp-dir]
@@ -53,11 +53,12 @@
   (let [[options [base-dir sample-name ref-file orig-ref-file] banner]
         (cli args
              ["-o" "--outdir" "Output directory" :default nil]
-             ["-t" "--tmpdir" "Temporary directory (defaults to output director)" :default nil])]
+             ["-d" "--tmpdir" "Temporary directory (defaults to output director)" :default nil]
+             ["-t", "--types", "Comma separate value of call types to include" :default "snp,indel,sv"])]
     (when (or (:help options) (some nil? [base-dir sample-name ref-file orig-ref-file]))
       (println "Required arguments:")
       (println "    <base-dir> VCF input file or Illumina directory to prepare.")
-      (println "    <sample-name> Genome reference file (GRCh37/b37 coordinates)")
+      (println "    <sample-name> Name of sample in newly prepared VCF file.")
       (println "    <ref-file> Genome reference file (GRCh37/b37 coordinates)")
       (println "    <orig-ref-file> Original genome reference file (hg19 coordinates)")
       (println)
@@ -65,7 +66,8 @@
       (System/exit 0))
     (let [out-dir (or (:outdir options) base-dir)
           tmp-dir (or (:tmpdir options) out-dir)
+          vtypes (map keyword (string/split (:types options) #","))
           out-file (prep-illumina-variants base-dir sample-name ref-file orig-ref-file
-                                           out-dir tmp-dir)]
+                                           out-dir tmp-dir vtypes)]
       (println out-file)
       (System/exit 0))))
